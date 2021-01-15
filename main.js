e4ca86f7e70edda77db0790377d1507881e5bf2b
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-01-15 21:39:03 +0900

    modify buttom

--- main.js
+++ main.js
@@ -8,29 +8,56 @@
 
   let startTime;
   let timeoutId;
+  let elapsedTime = 0;
 
   function countUp() {
-    const d = new Date(Date.now() - startTime);
-    const m = String(d.getMinutes()).padStart(2, "0");
-    const s = String(d.getSeconds()).padStart(2, "0");
-    const ms = String(d.getMilliseconds()).padStart(3, "0");
-    timer.textContent = `${m}:${s}.${ms}`;
+    const d = new Date(Date.now() - startTime + elapsedTime);
+    const h = String(d.getUTCHours()).padStart(1, "0");
+    const m = String(d.getMinutes()).padStart(1, "0");
+    const s = String(d.getSeconds()).padStart(1, "0");
+    const ms = String(d.getMilliseconds()).padStart(1, "0");
+    timer.textContent = `${h}:${m}:${s}:${ms}`;
 
     timeoutId = setTimeout(() => {
       countUp();
     }, 10);
   }
 
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+
+  function setButtonStateStopped() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  setButtonStateInitial();
+
   start.addEventListener("click", () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener("click", () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener("click", () => {
-    timer.textContent = "00:00.000";
+    setButtonStateInitial();
+    timer.textContent = "0:0:0:0";
+    elapsedTime = 0;
   });
 }
